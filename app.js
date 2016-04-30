4dc51c0a31f32e4fcb18a921f1f6ba3406f7a29a
Harrison Beckerich <hbeckeri@Harrisons-MacBook-Air.local>
2016-04-30 04:57:55 -0400

    I cant spell

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ app.get('/ambulance', function (req, res) {
     });
 });
 
-app.get('/distiction', function(req, res){
+app.get('/distinction', function(req, res){
     distinction.find({}, function (err, docs) {
         res.send(docs);
     });
