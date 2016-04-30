ed0f513f5cfe063f87bd6eaf36cf0fbf0903dc1f
Harrison Beckerich <hbeckeri@Harrisons-MacBook-Air.local>
2016-04-30 05:59:38 -0400

    stuff

--- app/models/distincitons.js
+++ app/models/distincitons.js
@@ -5,7 +5,23 @@ var Dist = new mongoose.Schema({
 	index: {
 		type: Number,
 		required: true
+	},
+	latitude1: {
+		type: Number,
+		required: true
+	},
+	longitude1: {
+		type: Number,
+		required: true
+	},
+	latitude2: {
+		type: Number,
+		required: true
+	},
+	longitude2: {
+		type: Number,
+		required: true
 	}
 });
 
-module.exports = mongoose.model('distinction', Dist);
+module.exports = mongoose.model('grids', Dist);
