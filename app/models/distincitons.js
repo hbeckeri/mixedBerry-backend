387a0a7c16fb8c155c40affb7f737258f5c40cba
Harrison Beckerich <hbeckeri@Harrisons-MacBook-Air.local>
2016-04-30 05:51:44 -0400

    stuff

--- app/models/distincitons.js
+++ app/models/distincitons.js
@@ -5,22 +5,6 @@ var Dist = new mongoose.Schema({
 	index: {
 		type: Number,
 		required: true
-	},
-	latitude1: {
-		type: Number,
-		required: true
-	},
-	longitude1: {
-		type: Number,
-		required: true
-	},
-	latitude2: {
-		type: Number,
-		required: true
-	},
-	longitude2: {
-		type: Number,
-		required: true
 	}
 });
 
