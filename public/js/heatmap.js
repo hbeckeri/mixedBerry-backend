68f42135ba9222cec8521f0502ed0d2fe8cca6d7
jwlehman93 <jeremylehman9@gmail.com>
2016-04-30 10:05:44 -0400

    heat map done

--- public/js/heatmap.js
+++ public/js/heatmap.js
@@ -1,15 +1,23 @@
-var heatmapData;
+var heatmapData = [];
 
 console.log('here');
 
 var url = 'http://ec2-52-35-101-218.us-west-2.compute.amazonaws.com';
 //get heatmap data
-$.get(url + '/heat', function(data) {
-	heatmapData = data;
-	for(var i = 1; i < data.length; i++) {
-		console.log(data[i]);
+console.log('here');
+$.get(url + '/heat', function(doc) {
+	console.log(doc);
+	for(var i = 1; i < doc.length; i++) {
+		console.log(doc[i]);
 
-		new google.maps.LatLng(data[i].latitude, data[i].longitude);
+		heatmapData.push(new google.maps.LatLng(doc[i]["latitude"], doc[i]["longitude"]));
+	}
+}).done(function(doc) {
+	for(var i = 1; i < doc.length; i++) {
+		console.log(doc[i]);
+
+		heatmapData.push(new google.maps.LatLng(doc[i]["latitude"], doc[i]["longitude"]));
+
 	}
 });
 
@@ -21,9 +29,10 @@ map = new google.maps.Map(document.getElementById('map'), {
     zoom: 11,
     mapTypeId: google.maps.MapTypeId.ROADMAP
 });
-
+console.log(heatmapData);
 var heatmap = new google.maps.visualization.HeatmapLayer({
-	data: heatmapData
+	data: heatmapData,
+	radius: 40
 });
 heatmap.setMap(map);
 
