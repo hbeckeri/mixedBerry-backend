2c5aa8c0dad848483a5e7f74c1a981c1816392e7
jwlehman93 <jeremylehman9@gmail.com>
2016-04-30 04:49:31 -0400

    format distinction

--- app/models/distincitons.js
+++ app/models/distincitons.js
@@ -3,26 +3,26 @@ var mongoose = require('mongoose');
 
 var Distinctions = new mongoose.Schema({
 	index: {
-		type: "int",
+		type: Number,
 		required: true
 	},
 	latitude1: {
-		type: Double,
+		type: Number,
 		required: true
 	},
 	longitude1: {
-		type: Double,
+		type: Number,
 		required: true
 	},
 	latitude2: {
-		type: Double,
+		type: Number,
 		required: true
 	},
 	longitude2: {
-		type: Double,
+		type: Number,
 		required: true
 	}
 	
 });
 
-module.exports = mongoose.model('Distinctions', Distinctions);
+module.exports = mongoose.model('distinction', Distinctions);
