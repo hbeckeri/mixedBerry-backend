6c67e83f43972c1f9e8dbac5dd7c523fbc6b09a7
David Bidle <djbidle@my1026.com>
2016-04-30 06:07:35 +0000

    stuff

---
+++ app.js
@@ -0,0 +1,16 @@
+/**
+ * Created by hbeckeri on 12/9/15.
+ */
+var express = require('express');
+var app = express();
+
+var PORT = 80;
+app.use(express.static(__dirname + '/public'));
+
+app.listen(PORT, function () {
+    console.log('MixedBerry Server: Listening on port %s', PORT);
+});
+
+app.get('/', function(req, res) {
+    res.sendfile(__dirname + '/public/index.html');
+});
